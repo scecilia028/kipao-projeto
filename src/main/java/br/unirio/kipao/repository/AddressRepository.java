c314e7d06d7439cfbbf892abb50c11c7c52553d2
Cecilia Araújo <scecilia028@gmail.com>
2022-08-08 12:14:37 -0300

    Delete src directory

--- src/main/java/br/unirio/kipao/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package br.unirio.kipao.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.unirio.kipao.model.Address;
-
-public interface AddressRepository extends CrudRepository<Address, Long> {
-
-}
--- src/main/java/br/unirio/kipao/repository/PersonalizationRepository.java
+++
@@ -1,9 +0,0 @@
-package br.unirio.kipao.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.unirio.kipao.model.Personalization;
-
-public interface PersonalizationRepository extends CrudRepository<Personalization, Long> {
-
-}
